1c3c9a521a6fa844471c982d747f8dbc4a74eba9
HelloVikram <vikram.kumar.cs.2024@gmail.com>
2024-05-07 18:36:36 +0530

    updated

--- chatapp_node/Backend.js/app.js
+++ chatapp_node/Backend.js/app.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const adminRoutes=require('./admin');
-const shopRoutes=require('.shop');
+const shopRoutes=require('./shop');
 const app=express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use('/admin',adminRoutes);
-app.use(shopRoutes);
+app.use('/shop',shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
 app.listen(3000);
